8a53a0dca1ad33dda299acb817e540e30362a9c2
Ben Parsell <ben.parsell1507@gmail.com>
2018-11-29 16:12:18 -0500

    Revert "Merge branch 'features/navbar' of https://github.com/benparsell/cis-371-project into features/navbar"
    This reverts commit 6de4d1d946330a3621b5289b902b81eab671b32a, reversing
    changes made to 6161527dc25ebb37b2bf6a6fa335b98170350f39.

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,5 @@
-$(window).on('load', function(){
-  $("body").on('click', '#sign-in-button', function (ev) {
-    ev.preventDefault();
-    console.log("yus")
-    $('#sign-in').toggleClass('active');
-  });
-
-  $("#log-in").on('click', function (ev) {
-    console.log("yus")
+$( document ).ready(function() {
+  $('#sign-in-button').click(function(){
     $('#sign-in').toggleClass('active');
   });
 });
